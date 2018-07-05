03dbdcf6595cf011987ed11c883947fa5ebac346
Yogesh <35971073+yogeshd278@users.noreply.github.com>
2018-07-05 16:35:45 +0530

    calculator.py

---
+++ code/python/day-0/calculator.py
@@ -0,0 +1,10 @@
+    # welcome to calculator program
+a, b = 5, 5
+print ("welcome to calculator program \n")
+num = int(input('enter the no between 1 to 4 for calculation and 0 for exit: '))
+if ( num == 1 ):  print (a+b)
+elif ( num == 2 ):  print (a-b)
+elif ( num == 3 ):  print (a*b)
+elif ( num == 4 ):  print (a/b)
+elif ( num == 0 ):  exit()
+            
