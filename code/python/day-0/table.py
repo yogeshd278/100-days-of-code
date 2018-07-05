3696f576c80b91ed16532835f9e80da36541e737
Yogesh <35971073+yogeshd278@users.noreply.github.com>
2018-07-05 16:36:11 +0530

    table.py

---
+++ code/python/day-0/table.py
@@ -0,0 +1,4 @@
+a, b, c, d, e, f, g, h, j, k = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+print ("display table 1 to 10\n")
+for i in range(1, 11):
+   print(a*i,'\t',b*i,'\t',c*i,'\t',d*i,'\t',e*i,'\t',f*i,'\t',g*i,'\t',h*i,'\t',j*i,'\t',k*i)
