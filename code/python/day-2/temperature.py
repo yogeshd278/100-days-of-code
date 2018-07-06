312c4d635705bd42e11d4dd8379d6a22e722030e
Yogesh <35971073+yogeshd278@users.noreply.github.com>
2018-07-06 13:35:01 +0530

    temperature.py

---
+++ code/python/day-2/temperature.py
@@ -0,0 +1,6 @@
+    # temperature program (celsius to fahrenheit)
+
+cel = input('enter the temperature in celsius ')
+celsius = float(cel)
+fahrenheit = (1.8*celsius) + 32
+print("Temperature in fahrenheit ",fahrenheit)
